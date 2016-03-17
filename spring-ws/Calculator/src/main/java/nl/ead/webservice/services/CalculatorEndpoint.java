9001fad1c694dee66bcede3293888331d2cc0177
Joost_Vissers <joostvissers_1709@hotmail.com>
2016-03-17 12:02:22 +0100

    hallo111

--- spring-ws/Calculator/src/main/java/nl/ead/webservice/services/CalculatorEndpoint.java
+++ spring-ws/Calculator/src/main/java/nl/ead/webservice/services/CalculatorEndpoint.java
@@ -27,28 +27,8 @@ public class CalculatorEndpoint {
         // Veranderen naar pay
         // a sequence of a minimum of 1 and unbounded max is generated as a
         // List<>
-        List<Integer> paramList = req.getInput().getParamlist().getParam();
-        CalculateOperation op = req.getInput().getOperation();
-        int retValue = paramList.get(0);
-        StringBuffer calculationInput = new StringBuffer();
-        calculationInput.append(paramList.get(0));
 
-        for (int i = 1; i < paramList.size(); i++) {
-            // CalculateOperation is generated as an enum
-            if (op.equals(CalculateOperation.ADD)) {
-                retValue += paramList.get(i).intValue();
-                calculationInput.append(" + " + paramList.get(i).intValue());
-            } else if (op.equals(CalculateOperation.SUBTRACT)) {
-                retValue -= paramList.get(i).intValue();
-                calculationInput.append(" -" + paramList.get(i).intValue());
-            } else if (op.equals(CalculateOperation.MULTIPLY)) {
-                retValue *= paramList.get(i).intValue();
-                calculationInput.append(" * " + paramList.get(i).intValue());
-            } else if (op.equals(CalculateOperation.DIVIDE)) {
-                retValue /= paramList.get(i).intValue();
-                calculationInput.append(" / " + paramList.get(i).intValue());
-            }
-        }
+        SubscriptionMethod method = req.getInput().getSubscriptionMethod();
 
         // Doe hier betaal dingen
         // Haal subscription request gegevens uit het request.
@@ -62,21 +42,52 @@ public class CalculatorEndpoint {
                 // Sla payment log op in database
                 // Ecrypt betaal gegevens en sla deze op
                 // Return bericht yay succes!
-            // Return bericht whops shit is is nie goe nie.
+                // Return bericht whops shit is is nie goe nie.
 
-        //Anders return bericht geen geldige methode opgegeven
+            //Anders return bericht geen geldige methode opgegeven
+
+        if(method.equals(SubscriptionMethod.PAY_PAL)){
+            FundingInstruments paramList = req.getInput().getPaypalParamlist().getFundingInstruments();
+        }
+        else if(method.equals(SubscriptionMethod.BITCOIN)){
+            //FundingInstruments paramList = req.getInput().getBitcoinParamlist();
+        }
+
+//        CalculateOperation op = req.getInput().getOperation();
+//        int retValue = paramList.get(0);
+//        StringBuffer calculationInput = new StringBuffer();
+//        calculationInput.append(paramList.get(0));
+//
+//        for (int i = 1; i < paramList.size(); i++) {
+//            // CalculateOperation is generated as an enum
+//            if (op.equals(CalculateOperation.ADD)) {
+//                retValue += paramList.get(i).intValue();
+//                calculationInput.append(" + " + paramList.get(i).intValue());
+//            } else if (op.equals(CalculateOperation.SUBTRACT)) {
+//                retValue -= paramList.get(i).intValue();
+//                calculationInput.append(" -" + paramList.get(i).intValue());
+//            } else if (op.equals(CalculateOperation.MULTIPLY)) {
+//                retValue *= paramList.get(i).intValue();
+//                calculationInput.append(" * " + paramList.get(i).intValue());
+//            } else if (op.equals(CalculateOperation.DIVIDE)) {
+//                retValue /= paramList.get(i).intValue();
+//                calculationInput.append(" / " + paramList.get(i).intValue());
+//            }
+//        }
+
+
 
         CalculateResult result = new CalculateResult();
-        result.setMessage("Here are the results of the jury for the calculation " + calculationInput);
-        result.setValue(retValue);
+//        result.setMessage("Here are the results of the jury for the calculation " + calculationInput);
+//        result.setValue(retValue);
         CalculateResponse resp = new CalculateResponse();
         resp.setResult(result);
 
         // OK, I know this isn't the best example of an external service for a calculator....
-        moviePrinter.printMovieDetails("Bond");
+        //moviePrinter.printMovieDetails("Bond");
 
-        Calculation calculation = new Calculation(calculationInput.toString(),retValue+"");
-        calculationDao.save(calculation);
+//         Calculation calculation = new Calculation(calculationInput.toString(),retValue+"");
+//        calculationDao.save(calculation);
 
         return resp;
     }
